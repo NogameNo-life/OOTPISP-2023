925d2fb75680811b008d867cef33f5a411cd33ef
AlexaBailey <alexaserko@gmail.com>
2023-10-15 19:44:20 +0300

    Add task_02

---
+++ trunk/PO210643/task_02/src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "texteditor.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TextEditor w;
+    w.show();
+    return a.exec();
+}
