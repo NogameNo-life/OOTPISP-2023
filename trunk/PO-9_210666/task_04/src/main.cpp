a57798d46d1a38136b68487153535a39aa81f1a9
Aleksey <11_alx_11@ro.ru>
2023-10-20 23:16:46 +0300

    escape code duplication

--- trunk/PO-9_210666/task_04/src/main.cpp
+++ trunk/PO-9_210666/task_04/src/main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "mainwindow.h" //a_task_04_kiril
 
-#include <QApplication>
+#include <QApplication> //a_task_04_kiril
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //a_task_04_kiril
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication a_task_04_kiril(argc, argv);
+    MainWindow w_task_04_kiril;
+    w_task_04_kiril.show();
+    return a_task_04_kiril.exec();
 }
