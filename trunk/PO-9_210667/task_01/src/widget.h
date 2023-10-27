c7c4ab9819895f46f915433ba0a92ce38a75c0a1
Vodemus <100290223+Vodemus@users.noreply.github.com>
2023-10-27 10:54:17 +0300

    Update trunk/PO-9_210667/task_01/src/widget.h
    Co-authored-by: 11ALX11 <91539438+11ALX11@users.noreply.github.com>

--- trunk/PO-9_210667/task_01/src/widget.h
+++ trunk/PO-9_210667/task_01/src/widget.h
@@ -1,21 +1,21 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
+#include <QWidget> //210667
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Widget; }
+namespace Ui { class Widget; } //210667
 QT_END_NAMESPACE
 
-class Widget : public QWidget
-{
+class Widget : public QWidget //210667
+{ //210667
     Q_OBJECT
 
-public:
-    Widget(QWidget *parent = nullptr);
-    ~Widget();
+public: //210667
+    Widget(QWidget *parent = nullptr); //210667
+    ~Widget(); //210667
 
-private:
-    Ui::Widget *ui;
+private: //210667
+    Ui::Widget *ui; //210667
 };
 #endif // WIDGET_H
