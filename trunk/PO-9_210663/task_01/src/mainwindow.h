d893b5eb1246f983199d22d24212d1be0b130941
nikalow1 <42293743+nikalow1@users.noreply.github.com>
2023-11-10 11:18:11 +0300

    fix_2 task_1

--- trunk/PO-9_210663/task_01/src/mainwindow.h
+++ trunk/PO-9_210663/task_01/src/mainwindow.h
@@ -3,19 +3,20 @@
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+QT_BEGIN_NAMESPACE //210663 mainwindow.h
+namespace Ui { class MainWindow; } //210663 mainwindow.h
+QT_END_NAMESPACE //210663 mainwindow.h
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow //210663 mainwindow.h
 {
-    Q_OBJECT
+    Q_OBJECT //210663 mainwindow.h
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow(QWidget *parent = nullptr); //210663 mainwindow.h
+    ~MainWindow(); //210663 mainwindow.h
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow *ui; //210663 mainwindow.h
 };
-#endif // MAINWINDOW_H
+#endif // MAINWINDOW_H 210663 mainwindow.h
+ 
