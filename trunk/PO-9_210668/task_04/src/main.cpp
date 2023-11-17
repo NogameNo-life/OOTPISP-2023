e9a56eb6e963ef35517d8edba0162ac6e50a78f5
dima <dmitriy.solyshko@mail.ru>
2023-11-17 09:11:51 +0300

    Update main.cpp

--- trunk/PO-9_210668/task_04/src/main.cpp
+++ trunk/PO-9_210668/task_04/src/main.cpp
@@ -1,10 +1,10 @@
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+#include "mainwindow.h" // 210668_main.cpp
+#include <QApplication> // 210668_main.cpp
+// 210668_main.cpp
+int main(int argc, char *argv[]) // 210668_main.cpp
+{ // 210668_main.cpp
+    QApplication a(argc, argv); // 210668_main.cpp
+    MainWindow w; // 210668_main.cpp
+    w.show(); // 210668_main.cpp
+    return a.exec(); // 210668_main.cpp
+} // 210668_main.cpp
