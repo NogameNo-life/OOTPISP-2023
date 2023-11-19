d7863028fd1793d2a1361f4d6195060601e6789b
Mkrshkk <Mkrshkk@users.noreply.github.com>
2023-11-19 21:58:22 +0300

    Fix duplication

--- trunk/PO-8_210641/task_04/src/main.cpp
+++ trunk/PO-8_210641/task_04/src/main.cpp
@@ -1,11 +1,10 @@
 #include "matrixdeterminant.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MatrixDeterminant w;
-    w.show();
+    MatrixDeterminant matrix_210641;
+    matrix_210641.show();
     return a.exec();
 }
