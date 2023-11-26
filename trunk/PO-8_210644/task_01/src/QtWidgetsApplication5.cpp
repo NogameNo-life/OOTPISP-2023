20ca3facdfdeecc3e172f23366ed223c1a9abc55
Semas2309 <99526629+Semas2309@users.noreply.github.com>
2023-11-26 14:51:56 +0300

    [task_01] Add task_01

---
+++ trunk/PO-8_210644/task_01/src/QtWidgetsApplication5.cpp
@@ -0,0 +1,10 @@
+#include "QtWidgetsApplication5.h"
+
+QtWidgetsApplication5::QtWidgetsApplication5(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+QtWidgetsApplication5::~QtWidgetsApplication5()
+{}
