409795e67cc6be85aed93164acad61015f136ddd
Anton Lobarev <loborew@gmail.com>
2023-11-28 09:08:04 +0300

    Add Lab_08

---
+++ trunk/PO-8_210640/task_08/Lab_08/Lab_08/Shapes/Basic Shapes/Ellipse.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab_08.Shapes.Basic_Shapes;
+
+public abstract class Ellipse
+{
+    public Ellipse()
+    {
+        Console.WriteLine("Ellipse");
+    }
+}
---
+++ trunk/PO-8_210640/task_08/Lab_08/Lab_08/Shapes/Basic Shapes/Line.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_08.Shapes.Basic_Shapes;
+
+
+public abstract class Line
+{
+    public Line()
+    {
+        Console.WriteLine("Line");
+    }
+}
