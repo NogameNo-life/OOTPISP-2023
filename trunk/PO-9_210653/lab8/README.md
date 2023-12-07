4e9735dded70c154ecd7266d8bc6cf5bc912b9f5
DarashkevichDmitriy <100340535+DarashkevichDmitriy@users.noreply.github.com>
2023-12-07 22:04:58 +0300

    [task_08]task_08

---
+++ trunk/PO-9_210653/lab8/README.md
@@ -0,0 +1,28 @@
+# Лабораторная работа № 8 #
+# Паттерны проектирования #
+# Цель работы #
+Изучить паттерны проектирования в С++.
+# Вариант 4 #
+4. Применить паттерн “Строитель” для построения
+
+    - Отчета по частям
+      - Части: Header – Заголовок, Block – Блок содержащий данные по отчету (результат SQL запроса), Ending – Концевик
+      - ConcreteBuilder: HtmlBuilder, TxtBuilder, XlsBuilder, DocBuilder
+    - Представления робота в игровой программе
+      - Части: Head, Body, Engine
+      - ConcreteBuilder выбрать самостоятельно
+    - Представления лица героя в ролевой игровой программе
+      - Части: Eyes  - параметры (Color, Figure), Nose (Color , Figure), Mouth (Color, Figure), Ears (Color, Figure), Hair (Color, Figure)
+      - ConcreteBuilder: UglyFaceBuilder, GoodFaceBuilder, SmileFaceBuilder
+# Демонстрация работы приложения #
+Отчёт по частям
+
+![img](img/1.jpg)
+
+Представления робота в игровой программе
+
+![img](img/1.jpg)
+
+Представления лица героя в ролевой игровой программе
+
+![img](img/1.jpg)
---
+++ trunk/PO-9_210653/lab8/src/main.cpp
@@ -0,0 +1,246 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <QString>
+
+// Интерфейс Строителя для отчета
+class ReportBuilder {
+public:
+    virtual void buildHeader() = 0;
+    virtual void buildBlock() = 0;
+    virtual void buildEnding() = 0;
+    virtual QString getReport() = 0;
+};
+
+// Конкретные строители отчета
+class HtmlBuilder : public ReportBuilder {
+public:
+    void buildHeader() override {
+        std::cout << "Create HTML Header" << std::endl;
+    }
+
+    void buildBlock() override {
+        std::cout << "Create HTML Block" << std::endl;
+    }
+
+    void buildEnding() override {
+        std::cout << "Create HTML Ending" << std::endl;
+    }
+
+    QString getReport() override {
+        return "Create HTML report";
+    }
+};
+
+class TxtBuilder : public ReportBuilder {
+public:
+    void buildHeader() override {
+        std::cout << "Create TXT Header" << std::endl;
+    }
+
+    void buildBlock() override {
+        std::cout << "Create TXT Block" << std::endl;
+    }
+
+    void buildEnding() override {
+        std::cout << "Create TXT Ending" << std::endl;
+    }
+
+    QString getReport() override {
+        return "Create TXT report";
+    }
+};
+
+class XlsBuilder : public ReportBuilder {
+public:
+    void buildHeader() override {
+        std::cout << "Create XLS Header" << std::endl;
+    }
+
+    void buildBlock() override {
+        std::cout << "Create XLS Block" << std::endl;
+    }
+
+    void buildEnding() override {
+        std::cout << "Create XLS Ending" << std::endl;
+    }
+
+    QString getReport() override {
+        return "Create XLS report";
+    }
+};
+
+class DocBuilder : public ReportBuilder {
+public:
+    void buildHeader() override {
+        std::cout << "Create DOC Header" << std::endl;
+    }
+
+    void buildBlock() override {
+        std::cout << "Create DOC Block" << std::endl;
+    }
+
+    void buildEnding() override {
+        std::cout << "Create DOC Ending" << std::endl;
+    }
+
+    QString getReport() override {
+        return "Create DOC report";
+    }
+};
+
+// Интерфейс Директора
+class ReportDirector {
+private:
+    ReportBuilder *builder;
+
+public:
+    ReportDirector(ReportBuilder *b) : builder(b) {}
+
+    void constructReport() {
+        builder->buildHeader();
+        builder->buildBlock();
+        builder->buildEnding();
+    }
+};
+
+// Представление робота с частями: Head, Body, Engine
+class Robot {
+public:
+    QString head;
+    QString body;
+    QString engine;
+};
+
+// Конкретные строители робота
+class RobotBuilder {
+public:
+    virtual void buildHead() = 0;
+    virtual void buildBody() = 0;
+    virtual void buildEngine() = 0;
+    virtual Robot getRobot() = 0;
+};
+
+class IronRobotBuilder : public RobotBuilder {
+private:
+    Robot robot;
+
+public:
+    void buildHead() override {
+        robot.head = "Iron Head";
+        std::cout << "Iron Head" << std::endl;
+    }
+
+    void buildBody() override {
+        robot.body = "Iron Body";
+        std::cout << "Iron Body" << std::endl;
+    }
+
+    void buildEngine() override {
+        robot.engine = "New Engine";
+        std::cout << "New Engine" << std::endl;
+    }
+
+    Robot getRobot() override {
+        return robot;
+    }
+};
+
+class Face {
+public:
+    QString eyes;
+    QString nose;
+    QString mouth;
+    QString ears;
+    QString hair;
+};
+
+class FaceBuilder {
+public:
+    virtual void buildEyes() = 0;
+    virtual void buildNose() = 0;
+    virtual void buildMouth() = 0;
+    virtual void buildEars() = 0;
+    virtual void buildHair() = 0;
+    virtual Face getFace() = 0;
+};
+
+class PersonFaceBuilder : public FaceBuilder {
+private:
+    Face face;
+
+public:
+    void buildEyes() override {
+        face.eyes = "Person's Eyes";
+        std::cout << "Person's Eyes" << std::endl;
+    }
+
+    void buildNose() override {
+        face.nose = "Person's Nose";
+        std::cout << "Person's Nose" << std::endl;
+    }
+
+    void buildMouth() override {
+        face.mouth = "Person's Mouth";
+        std::cout << "Person's Mouth" << std::endl;
+    }
+
+    void buildEars() override {
+        face.ears = "Person's Ears";
+        std::cout << "Person's Ears" << std::endl;
+    }
+
+    void buildHair() override {
+        face.hair = "Person's Hair";
+        std::cout << "Person's Hair" << std::endl;
+    }
+
+    Face getFace() override {
+        return face;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+
+    // Создание отчета в формате HTML
+    HtmlBuilder htmlBuilder;
+    ReportDirector htmlDirector(&htmlBuilder);
+    htmlDirector.constructReport();
+    QString htmlReport = htmlBuilder.getReport();
+
+    // Создание отчета в формате TXT
+    TxtBuilder txtBuilder;
+    ReportDirector txtDirector(&txtBuilder);
+    txtDirector.constructReport();
+    QString txtReport = txtBuilder.getReport();
+
+    // Создание отчета в формате XLS
+    XlsBuilder xlsBuilder;
+    ReportDirector xlsDirector(&xlsBuilder);
+    xlsDirector.constructReport();
+    QString xlsReport = xlsBuilder.getReport();
+
+    // Создание отчета в формате DOC
+    DocBuilder docBuilder;
+    ReportDirector docDirector(&docBuilder);
+    docDirector.constructReport();
+    QString docReport = docBuilder.getReport();
+
+    // Создание робота
+    IronRobotBuilder robotBuilder;
+    robotBuilder.buildHead();
+    robotBuilder.buildBody();
+    robotBuilder.buildEngine();
+    Robot IronRobot = robotBuilder.getRobot();
+
+    // Создание лица героя
+    PersonFaceBuilder personFaceBuilder;
+    personFaceBuilder.buildEyes();
+    personFaceBuilder.buildNose();
+    personFaceBuilder.buildMouth();
+    personFaceBuilder.buildEars();
+    personFaceBuilder.buildHair();
+    Face personFace = personFaceBuilder.getFace();
+
+    return a.exec();
+}
